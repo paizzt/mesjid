edccd82c9b13e9b9990b218b5615e1c2fe636e4d
paizzt <martevenfaisal@gmail.com>
2026-07-15 17:13:44 +0800

    s

--- backend/seeders/20260128000002-masjid.js
+++ backend/seeders/20260128000002-masjid.js
@@ -4,13 +4,13 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('masjid', [
       {
-        nama_masjid: 'Masjid Raya Al-Falah',
-        alamat: 'Jakarta Selatan, DKI Jakarta',
+        nama_masjid: 'Masjid sultan alauddin',
+        alamat: 'Makassar, Sulawesi Selatan',
         telepon: '081234567890',
         email: 'info@alfalah.com',
         ketua_pengurus: 'Reza Maulana',
         UserId: 1,
-        announcements: 'Selamat datang di Sistem Informasi Manajemen Masjid Raya Al-Falah.\nMohon jaga kebersihan area masjid.',
+        announcements: 'Selamat datang di Sistem Informasi Manajemen Masjid Sultan Alauddin.\nMohon jaga kebersihan area masjid.',
         countdown_duration: 600,
         createdAt: new Date(),
         updatedAt: new Date()
